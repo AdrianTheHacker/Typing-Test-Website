12f6435f072b46d7503f583d77eec8610869ef39
Adrian T (Big Coder Man) <adriantarantino2006@gmail.com>
2023-06-24 20:33:12 -0400

    Adding wrong key detection

--- src/typing-test-game/TypingTestGameComponents.js
+++ src/typing-test-game/TypingTestGameComponents.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useKeyPress } from './UseKeyPressed';
+import { useKeyPress } from './useKeyPress';
 import './TypingTestGameComponents.css';
 
 function TimeRemainingDisplay({ timeRemaining }) {
@@ -18,39 +18,52 @@ function WordsTypedDisplay({ wordsTyped }) {
     );
 }
 
-function WordsToType({ typingWords, typedWords }) {
+function WordsToType({ notTypedCharacters, correctCharacters, wrongCharacters }) {
     return(
         <div className='words-to-type-container'>
-            <span className='typed-words'>{typedWords}</span>
-            <span className='typing-words'>{typingWords}</span>
+            <span className='correct-characters'>{correctCharacters}</span>
+            <span className='wrong-characters'>{wrongCharacters}</span>
+            <span className='not-typed-characters'>{notTypedCharacters}</span>
         </div>
     )
 }
 
-let words = "React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.";
+const words = "React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.";
+
+const singleQuotationMark = "'";
+const allowedCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\;|:"<,>.?/ '.concat(singleQuotationMark);
 
 export function TypingTestBase() {
-    const [ wordsTyped, setWordsTyped ] = useState(0);
-    const [ typedWords, setTypedWords ] = useState('');
-    const [ typingWords, setTypingWords ] = useState(words);
+    const [ numberOfWordsTyped, setWordsTyped ] = useState(0);
+    const [ notTypedCharacters, setNotTypedCharacters ] = useState(words);
+    const [ correctCharacters, setCorrectCharacters ] = useState('');
+    const [ wrongCharacters, setWrongCharacters ] = useState('');
+    const [ wrongLetterTyped, setWrongLetterTyped ] = useState(false);
 
     useKeyPress(key => {
-        console.log(key);
 
-        if(key !== typingWords.charAt(0)) { return; }
-        if(key === ' ') { setWordsTyped(wordsTyped + 1); }
-        if(typingWords.length === 1) { setWordsTyped(wordsTyped + 1); }
+        
+        
+        if(!allowedCharacters.includes(key)) { return; }
 
-        console.log("Matching Letters!");
+        if(key !== notTypedCharacters.charAt(0)) { 
+            setWrongCharacters(wrongCharacters.concat(notTypedCharacters.charAt(0)));
+            setNotTypedCharacters(notTypedCharacters.slice(1));
+            setWrongLetterTyped(true);
+            return; 
+        }
 
-        setTypedWords(typedWords.concat(typingWords.charAt(0)));
-        setTypingWords(typingWords.slice(1));
+        if(key === ' ') { setWordsTyped(numberOfWordsTyped + 1); }
+        if(notTypedCharacters.length === 1) { setWordsTyped(numberOfWordsTyped + 1); }
+
+        setCorrectCharacters(correctCharacters.concat(notTypedCharacters.charAt(0)));
+        setNotTypedCharacters(notTypedCharacters.slice(1));
     }); 
 
     return(
         <div className='typing-test-base'>
-            <WordsToType typingWords={typingWords} typedWords={typedWords}/>
-            <WordsTypedDisplay wordsTyped={wordsTyped}/>
+            <WordsToType notTypedCharacters={notTypedCharacters} correctCharacters={correctCharacters} wrongCharacters={wrongCharacters}/>
+            <WordsTypedDisplay wordsTyped={numberOfWordsTyped}/>
             <TimeRemainingDisplay timeRemaining={30}/>
         </div>
     )
