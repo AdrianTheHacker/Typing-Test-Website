1054e984f18d1fa093bae6994e401bf951625d47
Adrian T (Big Coder Man) <adriantarantino2006@gmail.com>
2023-06-25 13:38:06 -0400

    Fixing WordCount Bug
    Now words count won't increase if wrong letters are typed

--- src/typing-test-game/TypingTestGameComponents.js
+++ src/typing-test-game/TypingTestGameComponents.js
@@ -69,7 +69,7 @@ function getWordsSample(words) {
 const words = 'a about all also and as at be because but by can come could day do even find first for from get give go have he her here him his how I if in into it its just know like look make man many me more my new no not now of on one only or other our out people say see she so some take tell than that the their them then there these they thing think this those time to two up use very want way we well what when which who will with would year you your';
 
 export function TypingTestBase() { 
-    const amountOfTime = 15;
+    const amountOfTime = 30;
 
     const [StartScreenModal, openStartScreen, closeStartScreen, isStartScreenOpen] = useModal('root', {
         preventScroll: true,
@@ -125,7 +125,7 @@ export function TypingTestBase() {
             return;
         }
 
-        if (key === ' ') { setWordsTyped(numberOfWordsTyped + 1); }
+        if (key === ' ' && wrongCharacters === '') { setWordsTyped(numberOfWordsTyped + 1); }
         if (notTypedCharacters.length === 1) { setWordsTyped(numberOfWordsTyped + 1); }
 
         if (wrongCharacters !== '') {
